a0b9274bc0e377e1d4a971f46b2e9f69dd6faba0
Rémi Vion <vion.remi@gmail.com>
2020-10-09 15:52:36 +0200

    setup page architecture

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,13 @@ import * as React from "react"
 import { observer } from "mobx-react-lite"
 
 export const App = observer(() => {
-    return <div>test</div>
+    return (
+        <div className="col grow">
+            <div className="page-header block">HEADER</div>
+            <div className="page-body grow row">
+                <div className="page-app1 block grow basis0"></div>
+                <div className="page-app2 block grow basis0"></div>
+            </div>
+        </div>
+    )
 })
